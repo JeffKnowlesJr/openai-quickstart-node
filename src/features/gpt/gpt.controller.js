0f60483bd7e7848a894cb1e4d17c425f69437c27
jeffknowlesjr <jeffknowlesjr@gmail.com>
2023-03-23 01:35:16 -0400

    gpt4 added feature

--- src/features/gpt/gpt.controller.js
+++ src/features/gpt/gpt.controller.js
@@ -1,6 +1,8 @@
 const openai = require('../../config/openai')
 const { createCompletion } = require('./gpt.service')
 
+const conversationHistory = new Map()
+
 module.exports = {
   handleMessage: async function (message, prefix) {
     if (
@@ -11,10 +13,32 @@ module.exports = {
       return
     }
 
-    const request = message.content
+    const channelId = message.channel.id
+
+    if (!conversationHistory.has(channelId)) {
+      conversationHistory.set(channelId, [])
+    }
+
+    const userMessage = message.content.slice(prefix.length).trim()
+    conversationHistory.get(channelId).push({
+      role: 'user',
+      content: userMessage
+    })
+
+    const request = conversationHistory
+      .get(channelId)
+      .map((message) => {
+        return `${message.role}: ${message.content}`
+      })
+      .join('\n')
 
     try {
       const response = await createCompletion(request, openai)
+      conversationHistory.get(channelId).push({
+        role: 'bot',
+        content: response
+      })
+
       message.channel.send(response)
     } catch (error) {
       if (error.response) {
--- src/features/gpt/gpt.service.js
+++ src/features/gpt/gpt.service.js
@@ -1,13 +1,17 @@
 module.exports = {
   createCompletion: async function (request, openai) {
+    const prompt = `Conversation:\n${request}\n\nbot:`
+
     const completion = await openai.createCompletion({
       model: 'text-davinci-003',
-      prompt: request,
+      prompt: prompt,
       max_tokens: 2048,
-      temperature: 0.6
+      temperature: 0.6,
+      n: 1,
+      stop: ['\n']
     })
 
-    const response = completion.data.choices[0].text
+    const response = completion.data.choices[0].text.trim()
     return response
   }
 }
