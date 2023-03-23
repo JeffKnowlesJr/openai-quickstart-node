768bce865e40b3c13d82011a0ef85e799093d4d4
jeffknowlesjr <jeffknowlesjr@gmail.com>
2023-03-23 02:43:18 -0400

    dead branch

--- src/features/gpt/gpt.controller.js
+++ src/features/gpt/gpt.controller.js
@@ -1,51 +1,85 @@
-const openai = require('../../config/openai')
-const { createCompletion } = require('./gpt.service')
+const { GatewayIntents } = require('discord.js')
+const { OpenAIApi, Configuration } = require('openai')
+const { updateConversationContext } = require('./gpt.service')
 
-const conversationHistory = new Map()
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
 
-module.exports = {
-  handleMessage: async function (message, prefix) {
-    if (
-      message.author.bot ||
-      !message.content.startsWith(prefix) ||
-      message.channel.type !== 0
-    ) {
-      return
-    }
+const openai = new OpenAIApi(configuration)
 
-    const channelId = message.channel.id
+const maxTokensPerMessage = 100
+const maxMessagesPerConversation = 10
+const conversationLengths = new Map()
 
-    if (!conversationHistory.has(channelId)) {
-      conversationHistory.set(channelId, [])
-    }
+module.exports = async (message) => {
+  const { author, channel, content, guild } = message
+  const { id: authorId } = author
+  const { id: channelId } = channel
+  const { id: guildId } = guild
 
-    const userMessage = message.content.slice(prefix.length).trim()
-    conversationHistory.get(channelId).push({
-      role: 'user',
-      content: userMessage
+  // Ignore messages from bots, non-text channels, and messages without prefix
+  if (
+    author.bot ||
+    !content.startsWith('!gpt') ||
+    channel.type !== 'GUILD_TEXT'
+  ) {
+    return
+  }
+
+  // Get conversation history for the user or channel
+  const conversationHistory = updateConversationContext({
+    authorId,
+    channelId,
+    content
+  })
+
+  // Create prompt for OpenAI API by concatenating conversation history and new message
+  const prompt = conversationHistory.join(' ')
+
+  // Get the length of the current conversation
+  let conversationLength = conversationLengths.get(authorId) || 0
+
+  // Check if the conversation has reached the token limit
+  if (conversationLength + prompt.split(' ').length > maxTokensPerMessage) {
+    console.log(`Token limit reached for conversation with user ${authorId}`)
+    channel.send(
+      "Sorry, I am out of tokens for this conversation. Let's chat later!"
+    )
+    return
+  }
+
+  // Call OpenAI API to generate response
+  try {
+    const completion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt,
+      max_tokens: maxTokensPerMessage,
+      temperature: 0.1
     })
 
-    const request = conversationHistory
-      .get(channelId)
-      .map((message) => {
-        return `${message.role}: ${message.content}`
-      })
-      .join('\n')
+    const response = completion.data.choices[0].text.trim()
 
-    try {
-      const response = await createCompletion(request, openai)
-      conversationHistory.get(channelId).push({
-        role: 'bot',
-        content: response
-      })
+    // Store the response in the conversation history
+    conversationHistory.push(response)
 
-      message.channel.send(response)
-    } catch (error) {
-      if (error.response) {
-        console.error(error.response.status, error.response.data)
-      } else {
-        console.error(`Error with OpenAI API request: ${error.message}`)
-      }
+    // Limit the conversation history to maxMessagesPerConversation messages
+    if (conversationHistory.length > maxMessagesPerConversation) {
+      conversationHistory.shift()
+    }
+
+    // Update the conversation length
+    conversationLength += prompt.split(' ').length + response.split(' ').length
+    conversationLengths.set(authorId, conversationLength)
+
+    // Send the generated response to the channel
+    channel.send(response)
+  } catch (error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data)
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`)
     }
   }
 }
--- src/features/gpt/gpt.service.js
+++ src/features/gpt/gpt.service.js
@@ -1,16 +1,69 @@
-module.exports = {
-  createCompletion: async function (request, openai) {
-    const prompt = `Conversation:\n${request}\n\nbot:`
+const { OpenAIApi, Configuration } = require('openai')
 
-    const completion = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: prompt,
-      max_tokens: 4096,
-      temperature: 0.6,
-      n: 1
-    })
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
 
-    const response = completion.data.choices[0].text.trim()
-    return response
+const openai = new OpenAIApi(configuration)
+
+/**
+ * Generates a response from the OpenAI API given the conversation history and a new message.
+ * @param {Array<string>} conversationHistory The conversation history.
+ * @param {string} messageContent The new message content.
+ * @param {number} maxTokensPerMessage The maximum number of tokens per message.
+ * @param {number} conversationLength The length of the conversation.
+ * @param {number} maxMessagesPerConversation The maximum number of messages per conversation.
+ * @returns {Promise<string>} A promise that resolves to the generated response.
+ */
+async function generateResponse(
+  conversationHistory,
+  messageContent,
+  maxTokensPerMessage,
+  conversationLength,
+  maxMessagesPerConversation
+) {
+  // Append the new message to the conversation history
+  conversationHistory.push(messageContent)
+
+  // Limit the conversation history to maxMessagesPerConversation messages
+  if (conversationHistory.length > maxMessagesPerConversation) {
+    conversationHistory.shift()
   }
+
+  // Update the conversation length
+  conversationLength += messageContent.split(' ').length
+
+  // Create prompt for OpenAI API by concatenating conversation history and new message
+  const prompt = conversationHistory.join(' ') + ' ' + messageContent
+
+  // Check if the conversation has reached the token limit
+  if (conversationLength > maxTokensPerMessage) {
+    throw new Error(
+      `Token limit reached for conversation: ${conversationHistory.join(' ')}`
+    )
+  }
+
+  // Call OpenAI API to generate response
+  const completion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: prompt,
+    max_tokens: maxTokensPerMessage - conversationLength,
+    temperature: 0.1
+  })
+
+  const response = completion.data.choices[0].text.trim()
+
+  // Store the response in the conversation history
+  conversationHistory.push(response)
+
+  // Limit the conversation history to maxMessagesPerConversation messages
+  if (conversationHistory.length > maxMessagesPerConversation) {
+    conversationHistory.shift()
+  }
+
+  return response
+}
+
+module.exports = {
+  generateResponse
 }
